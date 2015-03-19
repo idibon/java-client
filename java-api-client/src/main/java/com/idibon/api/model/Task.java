85c6ab325e591ae703686c7d3be7e502b03a93a5
Gary King <gary@idibon.com>
2015-03-19 08:09:09 -0700

    Revert accidental merge of rules commits

--- java-api-client/src/main/java/com/idibon/api/model/Task.java
+++ java-api-client/src/main/java/com/idibon/api/model/Task.java
@@ -3,17 +3,10 @@
  */
 package com.idibon.api.model;
 
-import java.util.*;
-
-import java.util.concurrent.Future;
-import java.util.concurrent.ExecutionException;
-
 import java.io.IOException;
 import javax.json.*;
 
 import com.idibon.api.http.HttpInterface;
-import com.idibon.api.model.Collection;
-import static com.idibon.api.model.Util.JSON_BF;
 
 /**
  * A machine learning task inside a Collection.
@@ -180,125 +173,6 @@ public class Task extends IdibonHash {
         return _parent;
     }
 
-    /**
-     * Returns all of the ML tuning rules defined for this task.
-     */
-    public Map<Label, List<TuningRules.Rule>> getRules() throws IOException {
-        return Collections.unmodifiableMap(getCachedTuningRules());
-    }
-
-    /**
-     * Adds new tuning rules to the task, or update the weights for existing
-     * rules.
-     *
-     * @param rules List of rules to add. If an identical rule already exists
-     *        in the task, the rules' weight will be updated to the new value.
-     */
-    public void addRules(TuningRules.Rule... rules) throws IOException {
-        TuningRules tuning = getCachedTuningRules().clone();
-
-        /* loop over all arguments; if the rule isn't found elsewhere in
-         * the existing dictionary, add it */
-        for (TuningRules.Rule rule : rules) {
-            if (!rule.label.getTask().equals(this))
-                throw new IllegalArgumentException("Wrong task on rule");
-
-            List<TuningRules.Rule> labelRules = tuning.get(rule.label);
-            if (labelRules == null) {
-                labelRules = new ArrayList<>();
-                tuning.put(rule.label, labelRules);
-            }
-
-            Iterator<TuningRules.Rule> existingRules = labelRules.iterator();
-            /* delete every rule for the same phrase as the new rule
-             * TODO: build a map of the new phrases as a pre-process, so this
-             * is O(N+M), rather than O(NM) */
-            while (existingRules.hasNext()) {
-                TuningRules.Rule existing = existingRules.next();
-                if (existing.phrase.equals(rule.phrase))
-                    existingRules.remove();
-            }
-            labelRules.add(rule);
-        }
-
-        JsonObject task = JSON_BF.createObjectBuilder()
-            .add("task", JSON_BF.createObjectBuilder()
-                 .add("config", Util.toJson(tuning)).build()).build();
-
-        Future<JsonValue> result = _httpIntf.httpPost(getEndpoint(), task);
-        try {
-            JsonValue v = result.get();
-            /* if the dictionary update was successful, overwrite the cached
-             * copy with the updated rules. */
-            _tuningRules = tuning;
-        } catch (ExecutionException ex) {
-            if (ex.getCause() instanceof IOException)
-                throw (IOException)ex.getCause();
-            throw new IOException("Error posting rules", ex.getCause());
-        } catch (InterruptedException ex) {}
-    }
-
-    /**
-     * Deletes one or more existing tuning rules.
-     *
-     * @param rules Array of tuning rules to delete from this task's tuning
-     *        dictionary. All of the rules must be for labels within this task.
-     */
-    public void deleteRules(TuningRules.Rule... rules) throws IOException {
-        TuningRules tuning = getCachedTuningRules().clone();
-        boolean dirty = false;
-
-        /* loop over all arguments, deleting each from the local tuning
-         * dictionary copy. */
-        for (TuningRules.Rule rule : rules) {
-            if (!rule.label.getTask().equals(this))
-                throw new IllegalArgumentException("Wrong task on rule");
-
-            List<TuningRules.Rule> labelRules = tuning.get(rule.label);
-            if (labelRules == null) continue;
-            Iterator<TuningRules.Rule> ruleIter = labelRules.iterator();
-
-            while (ruleIter.hasNext()) {
-                // Search for the first matching rule
-                TuningRules.Rule existing = ruleIter.next();
-                if (existing.equals(rule)) {
-                    // match found, delete it and quit the loop
-                    ruleIter.remove();
-                    dirty = true;
-                    break;
-                }
-            }
-        }
-
-        if (!dirty) return;  // no changes needed, don't upload
-
-        JsonObject task = JSON_BF.createObjectBuilder()
-            .add("task", JSON_BF.createObjectBuilder()
-                 .add("config", Util.toJson(tuning)).build()).build();
-        Future<JsonValue> result = _httpIntf.httpPost(getEndpoint(), task);
-        try {
-            JsonValue v = result.get();
-            /* if the dictionary update was successful, overwrite the cached
-             * copy with the updated rules. */
-            _tuningRules = tuning;
-        } catch (ExecutionException ex) {
-            if (ex.getCause() instanceof IOException)
-                throw (IOException)ex.getCause();
-            throw new IOException("Error posting rules", ex.getCause());
-        } catch (InterruptedException ex) {}
-
-    }
-
-    /**
-     * Forces cached JSON data to be reloaded from the server.
-     */
-    @SuppressWarnings("unchecked")
-    @Override public Task invalidate() {
-        super.invalidate();
-        _tuningRules = null;
-        return this;
-    }
-
     @Override public boolean equals(Object other) {
         if (other == this) return true;
         if (!(other instanceof Task)) return false;
@@ -316,7 +190,7 @@ public class Task extends IdibonHash {
         return new Task(name, parent, parent.getInterface());
     }
 
-    static Task instance(Collection parent, JsonObject obj) {
+     static Task instance(Collection parent, JsonObject obj) {
         String name = obj.getJsonObject("task").getString("name");
         return instance(parent, name).preload(obj);
     }
@@ -327,21 +201,6 @@ public class Task extends IdibonHash {
         _parent = parent;
     }
 
-    /**
-     * Private helper function to get the cached copy of the tuning rules,
-     * or create and cache an instance if one does not already exist.
-     */
-    private TuningRules getCachedTuningRules() throws IOException {
-        TuningRules tuning = _tuningRules;
-        if (tuning == null) {
-            tuning = TuningRules.parse(this,
-                getJson().getJsonObject(Keys.config.name()));
-            _tuningRules = tuning;
-        }
-        return tuning;
-    }
-
-    private volatile TuningRules _tuningRules;
     private final Collection _parent;
     private final String _name;
 }
